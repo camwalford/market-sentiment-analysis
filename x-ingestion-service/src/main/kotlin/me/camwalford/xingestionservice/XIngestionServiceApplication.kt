2187e83eab4e8841c36259f7a1e63689ae78ca9a
camwalford <cwalford2012@gmail.com>
2024-10-05 13:33:32 -0700

    Added the market-ingestion service with the spring boot plugin

---
+++ x-ingestion-service/src/main/kotlin/me/camwalford/xingestionservice/XIngestionServiceApplication.kt
@@ -0,0 +1,11 @@
+package me.camwalford.xingestionservice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class XIngestionServiceApplication
+
+fun main(args: Array<String>) {
+	runApplication<XIngestionServiceApplication>(*args)
+}
