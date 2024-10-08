fa79934311f7d74a22c93592037025f62ed890e0
camwalford <cwalford2012@gmail.com>
2024-10-07 17:19:14 -0700

    Fixed logic in MarketNewsProducerTest.kt

--- finnhub-ingestion-service/src/test/kotlin/me/camwalford/finnhubingestionservice/service/MarketNewsProducerTest.kt
+++ finnhub-ingestion-service/src/test/kotlin/me/camwalford/finnhubingestionservice/service/MarketNewsProducerTest.kt
@@ -1,33 +1,62 @@
-import me.camwalford.finnhubingestionservice.model.protobuf.MarketNewsList
-import me.camwalford.finnhubingestionservice.service.MarketNewsProducer
-import org.junit.jupiter.api.extension.ExtendWith
-import org.mockito.InjectMocks
-import org.mockito.Mock
-import org.mockito.Mockito.verify
-import org.mockito.junit.jupiter.MockitoExtension
-import org.springframework.kafka.core.KafkaTemplate
-import kotlin.test.Test
+package me.camwalford.finnhubingestionservice.service
 
-@ExtendWith(MockitoExtension::class)
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.MockK
+import io.mockk.verify
+import me.camwalford.finnhubingestionservice.model.protobuf.MarketNewsList
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.producer.RecordMetadata
+import org.apache.kafka.common.TopicPartition
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.support.SendResult
+import java.util.concurrent.CompletableFuture
+
 class MarketNewsProducerTest {
 
-  @Mock
+  @MockK
   private lateinit var kafkaTemplate: KafkaTemplate<String, MarketNewsList>
 
-  @InjectMocks
   private lateinit var marketNewsProducer: MarketNewsProducer
 
+  private val topic = "market-news-topic"
+  private val key = "general"
+
+  @BeforeEach
+  fun setUp() {
+    MockKAnnotations.init(this)
+    marketNewsProducer = MarketNewsProducer(kafkaTemplate, topic, key)
+  }
+
   @Test
-  fun `should send market news to Kafka`() {
+  fun `test sendMarketNews successfully`() {
     // Arrange
-    val topic = "market_news_topic"
-    val key = "news_key"
     val marketNewsList = MarketNewsList.newBuilder().build()
+    val sendResult = SendResult(ProducerRecord(topic, key, marketNewsList), RecordMetadata(TopicPartition(topic, 0), 0, 0, 0, 0, 0))
+    val future = CompletableFuture.completedFuture(sendResult)
+    every { kafkaTemplate.send(topic, key, marketNewsList) } returns future
 
     // Act
     marketNewsProducer.sendMarketNews(marketNewsList)
 
     // Assert
-    verify(kafkaTemplate).send(topic, key, marketNewsList)
+    verify { kafkaTemplate.send(topic, key, marketNewsList) }
+  }
+
+  @Test
+  fun `test sendMarketNews when KafkaTemplate throws exception`() {
+    // Arrange
+    val marketNewsList = MarketNewsList.newBuilder().build()
+    val future = CompletableFuture<SendResult<String, MarketNewsList>>()
+    future.completeExceptionally(RuntimeException("Kafka error"))
+    every { kafkaTemplate.send(topic, key, marketNewsList) } returns future
+
+    // Act
+    marketNewsProducer.sendMarketNews(marketNewsList)
+
+    // Assert
+    verify { kafkaTemplate.send(topic, key, marketNewsList) }
   }
 }
