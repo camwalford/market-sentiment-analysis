44fc9a78ab9f4294f0825495ebb70e39f8964a35
camwalford <cwalford2012@gmail.com>
2024-11-03 23:20:15 -0800

    Added line breaks

--- backend-api-service/src/main/kotlin/me/camwalford/backendapiservice/service/TokenService.kt
+++ backend-api-service/src/main/kotlin/me/camwalford/backendapiservice/service/TokenService.kt
@@ -30,21 +30,26 @@ class TokenService(
             .and()
             .signWith(secretKey)
             .compact()
+
     fun isValid(token: String, userDetails: UserDetails): Boolean {
         val email = extractEmail(token)
         return userDetails.username == email && !isExpired(token)
     }
+
     fun extractEmail(token: String): String? =
         getAllClaims(token)
             .subject
+
     fun isExpired(token: String): Boolean =
         getAllClaims(token)
             .expiration
             .before(Date(System.currentTimeMillis()))
+
     private fun getAllClaims(token: String): Claims {
         val parser = Jwts.parser()
             .verifyWith(secretKey)
             .build()
+
         return parser
             .parseSignedClaims(token)
             .payload
