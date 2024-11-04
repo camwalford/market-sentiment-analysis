1e300746c1ed5d43c55cc7904e7fce463d9f2863
camwalford <cwalford2012@gmail.com>
2024-11-03 22:11:43 -0800

    renamed method to use email

--- backend-api-service/src/main/kotlin/me/camwalford/backendapiservice/repository/UserRepository.kt
+++ backend-api-service/src/main/kotlin/me/camwalford/backendapiservice/repository/UserRepository.kt
@@ -1,13 +1,11 @@
 package me.camwalford.backendapiservice.repository
 
-// src/main/kotlin/com/example/demo/repository/UserRepository.kt
-
 
 import me.camwalford.backendapiservice.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface UserRepository : JpaRepository<User, Long> {
-    fun findByUsername(username: String): User?
+interface UserRepository : JpaRepository<User, Long>{
+    fun findByEmail(email: String): User?
 }
