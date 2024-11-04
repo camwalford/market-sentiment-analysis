15ecbd320105d5866f0d0a9e71a93515bf4ee073
camwalford <cwalford2012@gmail.com>
2024-11-03 23:20:02 -0800

    Added logout request matcher

--- backend-api-service/src/main/kotlin/me/camwalford/backendapiservice/config/SecurityConfiguration.kt
+++ backend-api-service/src/main/kotlin/me/camwalford/backendapiservice/config/SecurityConfiguration.kt
@@ -25,7 +25,7 @@ class SecurityConfiguration(
             .csrf { it.disable() }
             .authorizeHttpRequests {
                 it
-                    .requestMatchers("/api/auth", "api/auth/refresh", "/error")
+                    .requestMatchers("/api/auth", "api/auth/refresh", "/error", "/api/auth/logout")
                     .permitAll()
                     .requestMatchers(HttpMethod.POST, "/api/user")
                     .permitAll()
