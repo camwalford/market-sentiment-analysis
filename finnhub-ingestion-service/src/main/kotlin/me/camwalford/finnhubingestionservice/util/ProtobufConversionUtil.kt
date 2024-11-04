c8a09ed33359ba50d24cb69a21b2fad48904ea47
camwalford <cwalford2012@gmail.com>
2024-11-03 17:13:04 -0800

    Added method to convert single company news entries to protobuf.

--- finnhub-ingestion-service/src/main/kotlin/me/camwalford/finnhubingestionservice/util/ProtobufConversionUtil.kt
+++ finnhub-ingestion-service/src/main/kotlin/me/camwalford/finnhubingestionservice/util/ProtobufConversionUtil.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.protobuf.util.JsonFormat
 import me.camwalford.finnhubingestionservice.model.protobuf.CompanyNewsList
+import me.camwalford.finnhubingestionservice.model.protobuf.CompanyNewsProto
 import org.slf4j.LoggerFactory
 
 object ProtobufConversionUtil {
@@ -27,6 +28,18 @@ object ProtobufConversionUtil {
 
             logger.info("Parsing JSON to Protobuf")
             JsonFormat.parser().merge(jsonString, builder)
+            builder.build()
+        } catch (e: Exception) {
+            logger.error("Error parsing JSON to Protobuf: ${e.message}", e)
+            throw RuntimeException("Error parsing JSON to Protobuf", e)
+        }
+    }
+
+    fun convertCompanyNews(jsonNews: String): CompanyNewsProto {
+        val builder = CompanyNewsProto.newBuilder()
+        return try {
+            logger.info("Converting JSON to Protobuf for CompanyNews")
+            JsonFormat.parser().merge(jsonNews, builder)
             builder.build()
         } catch (e: Exception) {
             logger.error("Error parsing JSON to Protobuf: ${e.message}", e)
