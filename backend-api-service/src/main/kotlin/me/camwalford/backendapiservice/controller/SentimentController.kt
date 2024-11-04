678beb1d8d538ca9f3b42120a7e5f220e282869a
camwalford <cwalford2012@gmail.com>
2024-11-03 22:45:08 -0800

    Adjusted SentimentController.kt to use /api/sentiment as its base endpoint

--- backend-api-service/src/main/kotlin/me/camwalford/backendapiservice/controller/SentimentController.kt
+++ backend-api-service/src/main/kotlin/me/camwalford/backendapiservice/controller/SentimentController.kt
@@ -1,10 +1,14 @@
 package me.camwalford.backendapiservice.controller
 
+import me.camwalford.backendapiservice.service.SentimentService
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 
 @RestController
-@RequestMapping("/sentiment")
-class SentimentController {
+@RequestMapping("/api/sentiment")
+class SentimentController(
+    private val sentimentService: SentimentService
+) {
+
 }
