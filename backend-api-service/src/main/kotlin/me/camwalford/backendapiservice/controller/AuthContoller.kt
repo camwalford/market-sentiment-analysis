5fe96148bfd02a72a38ef7b8248ba4d9d8deff6c
camwalford <cwalford2012@gmail.com>
2024-11-03 22:38:32 -0800

    Added authenticate and refresh token methods

--- backend-api-service/src/main/kotlin/me/camwalford/backendapiservice/controller/AuthContoller.kt
+++ backend-api-service/src/main/kotlin/me/camwalford/backendapiservice/controller/AuthContoller.kt
@@ -1,13 +1,38 @@
 package me.camwalford.backendapiservice.controller
 
-import me.camwalford.backendapiservice.repository.UserRepository
-import org.springframework.beans.factory.annotation.Autowired
+
+import me.camwalford.backendapiservice.dto.AuthenticationRequest
+import me.camwalford.backendapiservice.dto.AuthenticationResponse
+import me.camwalford.backendapiservice.dto.RefreshTokenRequest
+import me.camwalford.backendapiservice.dto.TokenResponse
+import me.camwalford.backendapiservice.service.AuthenticationService
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.server.ResponseStatusException
 
 @RestController
-@RequestMapping("/auth")
-class AuthContoller {
-    @Autowired
-    private lateinit var userRepository: UserRepository
+@RequestMapping("/api/auth")
+class AuthController(
+    private val authenticationService: AuthenticationService
+) {
+    @PostMapping
+    fun authenticate(
+        @RequestBody authRequest: AuthenticationRequest
+    ): AuthenticationResponse =
+        authenticationService.authentication(authRequest)
+
+    @PostMapping("/refresh")
+    fun refreshAccessToken(
+        @RequestBody request: RefreshTokenRequest
+    ): TokenResponse =
+        authenticationService.refreshAccessToken(request.token)
+            ?.mapToTokenResponse()
+            ?: throw ResponseStatusException(HttpStatus.FORBIDDEN, "Invalid refresh token.")
+    private fun String.mapToTokenResponse(): TokenResponse =
+        TokenResponse(
+            token = this
+        )
 }
