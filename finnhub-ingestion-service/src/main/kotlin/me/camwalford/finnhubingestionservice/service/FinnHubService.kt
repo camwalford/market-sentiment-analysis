c4b8990d1426114394702d9d7ce79c158c403ff9
camwalford <cwalford2012@gmail.com>
2024-11-03 17:14:16 -0800

    Adjusted service to serve list of individual protobuf CompanyNewsProto objects instead of a serialized list object.

--- finnhub-ingestion-service/src/main/kotlin/me/camwalford/finnhubingestionservice/service/FinnHubService.kt
+++ finnhub-ingestion-service/src/main/kotlin/me/camwalford/finnhubingestionservice/service/FinnHubService.kt
@@ -1,8 +1,9 @@
 package me.camwalford.finnhubingestionservice.service
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.finnhub.api.models.CompanyNews
 import me.camwalford.finnhubingestionservice.client.FinnHubClient
-import me.camwalford.finnhubingestionservice.model.protobuf.CompanyNewsList
+import me.camwalford.finnhubingestionservice.model.protobuf.CompanyNewsProto
 import me.camwalford.finnhubingestionservice.util.ProtobufConversionUtil
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
@@ -13,19 +14,22 @@ class FinnHubService(private val finnHubClient: FinnHubClient) {
     private val logger = LoggerFactory.getLogger(FinnHubService::class.java)
     private val objectMapper = ObjectMapper()
 
-    fun fetchCompanyNewsList(company: String, from: String, to: String): CompanyNewsList {
-        logger.info("Fetching market news from FinnHubClient for category: $company, from: $from, to: $to")
+    fun fetchCompanyNewsList(company: String, from: String, to: String): List<CompanyNewsProto> {
+        logger.info("Fetching market news from FinnHubClient for company: $company, from: $from, to: $to")
         val companyNewsList = finnHubClient.getCompanyNewsList(company, from, to)
 
         return try {
-            val jsonString = objectMapper.writeValueAsString(companyNewsList)
-            val protobufData = ProtobufConversionUtil.convertCompanyNewsList(jsonString)
-            logger.info("Successfully converted company news to Protobuf format")
-            return protobufData
+            // Convert JSON list to individual Protobuf CompanyNewsProto objects
+            return companyNewsList.map { newsJson ->
+                val jsonString = objectMapper.writeValueAsString(newsJson)
+                ProtobufConversionUtil.convertCompanyNews(jsonString)
+            }.also {
+                logger.info("Successfully converted company news to individual Protobuf format")
+            }
         } catch (e: Exception) {
             logger.error("Failed to convert company news data to Protobuf", e)
             throw RuntimeException("Failed to convert JSON to Protobuf", e)
         }
-
     }
 }
+
