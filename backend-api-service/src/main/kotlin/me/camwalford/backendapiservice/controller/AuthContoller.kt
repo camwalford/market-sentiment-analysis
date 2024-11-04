80bbd7f15dfa33bf3c149b49baba54eeb9a9ab74
camwalford <cwalford2012@gmail.com>
2024-11-03 23:18:02 -0800

    Added logout endpoint

--- backend-api-service/src/main/kotlin/me/camwalford/backendapiservice/controller/AuthContoller.kt
+++ backend-api-service/src/main/kotlin/me/camwalford/backendapiservice/controller/AuthContoller.kt
@@ -5,8 +5,10 @@ import me.camwalford.backendapiservice.dto.AuthenticationRequest
 import me.camwalford.backendapiservice.dto.AuthenticationResponse
 import me.camwalford.backendapiservice.dto.RefreshTokenRequest
 import me.camwalford.backendapiservice.dto.TokenResponse
+import me.camwalford.backendapiservice.repository.RefreshTokenRepository
 import me.camwalford.backendapiservice.service.AuthenticationService
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -16,7 +18,9 @@ import org.springframework.web.server.ResponseStatusException
 @RestController
 @RequestMapping("/api/auth")
 class AuthController(
-    private val authenticationService: AuthenticationService
+    private val authenticationService: AuthenticationService,
+    private val refreshTokenRepository: RefreshTokenRepository
+
 ) {
     @PostMapping
     fun authenticate(
@@ -31,8 +35,15 @@ class AuthController(
         authenticationService.refreshAccessToken(request.token)
             ?.mapToTokenResponse()
             ?: throw ResponseStatusException(HttpStatus.FORBIDDEN, "Invalid refresh token.")
+
     private fun String.mapToTokenResponse(): TokenResponse =
         TokenResponse(
             token = this
         )
+
+    @PostMapping("/logout")
+    fun logout(@RequestBody request: RefreshTokenRequest): ResponseEntity<Void> {
+        refreshTokenRepository.deleteByToken(request.token)
+        return ResponseEntity.noContent().build()
+    }
 }
