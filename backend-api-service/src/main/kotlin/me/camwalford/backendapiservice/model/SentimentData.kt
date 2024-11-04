54a03e3f312ad3db4cde4fd3b62aca86630a5210
camwalford <cwalford2012@gmail.com>
2024-11-03 19:46:29 -0800

    Added sentimentdata and sentimentdata repository

---
+++ backend-api-service/src/main/kotlin/me/camwalford/backendapiservice/model/SentimentData.kt
@@ -0,0 +1,32 @@
+package me.camwalford.backendapiservice.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Index
+import jakarta.persistence.Table
+import java.time.LocalDate
+
+
+@Entity
+@Table(
+    name = "sentiment_data",
+    indexes = [
+        Index(name = "ticker_date_index", columnList = "ticker, date")
+    ]
+)
+data class SentimentData(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0,
+
+    @Column(nullable = false)
+    val ticker: String,
+
+    @Column(nullable = false)
+    val date: LocalDate,
+
+    @Column(nullable = false)
+    val sentiment: Double
+)
