d0bfb48b802a9de9b4ac64377988b66b2679a3bf
camwalford <cwalford2012@gmail.com>
2024-11-03 21:55:35 -0800

    Added more authentication configuration

--- backend-api-service/src/main/kotlin/me/camwalford/backendapiservice/config/Configuration.kt
+++ backend-api-service/src/main/kotlin/me/camwalford/backendapiservice/config/Configuration.kt
@@ -1,7 +1,17 @@
 package me.camwalford.backendapiservice.config
 
+import me.camwalford.backendapiservice.repository.UserRepository
+import me.camwalford.backendapiservice.service.CustomUserDetailsService
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Bean
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.authentication.AuthenticationProvider
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
 
 /**
  * Configuration class for the application. Enables JWT configuration properties to be loaded from the application.yml.
@@ -10,4 +20,22 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 @EnableConfigurationProperties(JwtProperties::class)
-class Configuration
+class Configuration {
+    @Bean
+    fun userDetailsService(userRepository: UserRepository): UserDetailsService =
+        CustomUserDetailsService(userRepository)
+
+    @Bean
+    fun encoder(): PasswordEncoder = BCryptPasswordEncoder()
+
+    @Bean
+    fun authenticationProvider(userRepository: UserRepository): AuthenticationProvider =
+        DaoAuthenticationProvider()
+            .also {
+                it.setUserDetailsService(userDetailsService(userRepository))
+                it.setPasswordEncoder(encoder())
+            }
+    @Bean
+    fun authenticationManager(config: AuthenticationConfiguration): AuthenticationManager =
+        config.authenticationManager
+}
