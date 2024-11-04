5c2e4d44979eced407b73b8ffe82606aa8bfbf0b
camwalford <cwalford2012@gmail.com>
2024-11-03 23:20:49 -0800

    Changed UserController.kt endpoint to "/api/user"

--- backend-api-service/src/main/kotlin/me/camwalford/backendapiservice/controller/UserController.kt
+++ backend-api-service/src/main/kotlin/me/camwalford/backendapiservice/controller/UserController.kt
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/api/user")
 class UserController(
     private val userService: UserService
 ) {
