fb07c42448b7e0acaaaa829b6b2c31345b69d10d
camwalford <cwalford2012@gmail.com>
2024-11-04 15:49:17 -0800

    Added debug logging to CustomUserDetailsService.kt in order to verify the data being sent to the DB

--- backend-api-service/src/main/kotlin/me/camwalford/backendapiservice/service/CustomUserDetailsService.kt
+++ backend-api-service/src/main/kotlin/me/camwalford/backendapiservice/service/CustomUserDetailsService.kt
@@ -1,6 +1,8 @@
 package me.camwalford.backendapiservice.service
 
 import me.camwalford.backendapiservice.repository.UserRepository
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
@@ -13,16 +15,31 @@ typealias ApplicationUser = me.camwalford.backendapiservice.model.User
 class CustomUserDetailsService(
     private val userRepository: UserRepository
 ) : UserDetailsService {
-    override fun loadUserByUsername(username: String): UserDetails =
-        userRepository.findByEmail(username)
+    private val logger: Logger = LoggerFactory.getLogger(CustomUserDetailsService::class.java)
+
+    override fun loadUserByUsername(username: String): UserDetails {
+        logger.info("Loading user by username: $username")
+        val user = userRepository.findByEmail(username)
             ?.mapToUserDetails()
-            ?: throw UsernameNotFoundException("User not found")
+            ?: run {
+                logger.warn("User not found with username: $username")
+                throw UsernameNotFoundException("User not found")
+            }
+        logger.debug(
+            "Loaded user details: username={}, password={}, authorities={}",
+            user.username,
+            user.password,
+            user.authorities
+        )
+        return user
+    }
 
-
-    private fun ApplicationUser.mapToUserDetails(): UserDetails =
-        User.builder()
+    private fun ApplicationUser.mapToUserDetails(): UserDetails {
+        logger.debug("Mapping ApplicationUser to UserDetails for user: ${this.email}")
+        return User.builder()
             .username(this.email)
             .password(this.password)
             .authorities(this.role.name)
             .build()
+    }
 }
