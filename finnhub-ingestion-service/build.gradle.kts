cf0a09bbc81c7e748d692d0fc965faf4f69315b9
camwalford <cwalford2012@gmail.com>
2024-11-10 18:07:47 -0800

    Removed proto dependencies and file

--- finnhub-ingestion-service/build.gradle.kts
+++ finnhub-ingestion-service/build.gradle.kts
@@ -6,7 +6,6 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.3.4"
 	id("io.spring.dependency-management") version "1.1.6"
-	id("com.google.protobuf") version "0.9.4"
 }
 
 group = "me.camwalford"
@@ -30,14 +29,10 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springframework.kafka:spring-kafka")
-	implementation("com.google.protobuf:protobuf-kotlin:3.25.1")
-	implementation("com.google.protobuf:protobuf-java-util:3.14.0")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("io.finnhub:kotlin-client:2.0.20")
 	implementation("io.github.cdimascio:dotenv-kotlin:6.4.0")
-	implementation("io.confluent:kafka-protobuf-serializer:7.5.1")
 	implementation("org.springframework.retry:spring-retry")
-
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	testImplementation("io.mockk:mockk:1.13.12")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
@@ -62,17 +57,4 @@ tasks.test {
 		"-XX:+EnableDynamicAgentLoading",
 		"-Djdk.instrument.traceUsage=false"
 	)
-}
-
-protobuf {
-	protoc {
-		artifact = "com.google.protobuf:protoc:3.25.1"
-	}
-	generateProtoTasks {
-		all().forEach { task ->
-			task.builtins {
-				create("kotlin")
-			}
-		}
-	}
 }
