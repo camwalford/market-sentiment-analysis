97f995462898858b54d1dd3f0b7aa0e3f915e7b0
camwalford <cwalford2012@gmail.com>
2024-11-13 18:21:33 -0800

    Fixed some endpoint/port issues

--- BERT-sentiment-analysis-service/app/config/settings.py
+++ BERT-sentiment-analysis-service/app/config/settings.py
@@ -3,7 +3,7 @@ from typing import List
 
 class Settings(BaseSettings):
     finbert_model_path: str = "ProsusAI/finbert"
-    allowed_origins: List[str] = ["http://localhost:8080", "http://localhost:8081"]
+    allowed_origins: List[str] = ["http://localhost:8080", "http://localhost:8081, http://0.0.0.0:8080"]
     port: int = 8082
 
     class Config:
